759907068188240988ebfb18e3c82666f2e90288
Artem <svetilov.art@gmail.com>
2023-04-03 20:44:30 +0300

    added some corrections

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -33,13 +33,12 @@ const Form = () => {
     
   return (
     <>
-        {showForm && <form action="#" className={styles.form} onSubmit={onSubmit}>
+        {showForm ? <form action="#" className={styles.form} onSubmit={onSubmit}>
             <InputBlock text={'How many inputs?'} value={inputs} setValue={setInputs} />
             <InputBlock text='How many textareas?' value={textareas} setValue={setTextareas} />
             <InputBlock text='How many checkboxes?' value={checkboxes} setValue={setCheckboxes} />
             <button type='submit'>BUILD</button>
-        </form>}
-        <CreatedForm data={data} />
+        </form> : <CreatedForm data={data} />} 
     </>
   )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,9 +5,13 @@ import styles from './Main.module.scss'
 
 const Main = () => {
   return (
-    <div className={styles.button}>
+    <div className={styles.container}>
+      <h2>Create Form App</h2>
+      <div className={styles.button}>
         <NavLink to='/form'>FORM</NavLink>
+      </div>
     </div>
+
   )
 }
 
